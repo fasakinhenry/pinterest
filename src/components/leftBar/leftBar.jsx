f8e83e369c9666d5905a796db0a60cb9752a640d
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-28 14:52:36 +0100

    feat: create and style the leftbar

--- src/components/leftBar/leftBar.jsx
+++ src/components/leftBar/leftBar.jsx
@@ -1,9 +1,30 @@
-import './leftBar.css'
+import './leftBar.css';
 
 const LeftBar = () => {
   return (
-    <div className='leftBar'>LeftBar</div>
-  )
-}
+    <div className='leftBar'>
+      <div className='menuIcons'>
+        <a href='/' className='menuIcon'>
+          <img src='/general/logo.png' alt='logo' className='logo' />
+        </a>
+        <a href='/' className='menuIcon'>
+          <img src='/general/home.svg' alt='home' />
+        </a>
+        <a href='/' className='menuIcon'>
+          <img src='/general/create.svg' alt='create' />
+        </a>
+        <a href='/' className='menuIcon'>
+          <img src='/general/updates.svg' alt='updates' />
+        </a>
+        <a href='/' className='menuIcon'>
+          <img src='/general/messages.svg' alt='messages' />
+        </a>
+      </div>
+      <a href='/' className='menuIcon'>
+        <img src='/general/messages.svg' alt='messages' />
+      </a>
+    </div>
+  );
+};
 
-export default LeftBar
+export default LeftBar;
