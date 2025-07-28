b5c7a57352c13170caf17a469636bd4223d00d93
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-28 14:58:26 +0100

    feat: create a pseudo topbar component

--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -1,7 +1,17 @@
 import './topBar.css';
 
 const TopBar = () => {
-  return <div className='topBar'>TopBar</div>;
+  return (
+    <div className='topBar'>
+      {/* Search */}
+      <div className='search'>
+        <img src='/general/search.svg' alt='search' />
+        <input type='text' placeholder='search' />
+      </div>
+      {/* User */}
+      <div className=''></div>
+    </div>
+  );
 };
 
 export default TopBar;
