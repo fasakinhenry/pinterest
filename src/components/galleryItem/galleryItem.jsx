bf4f36c5f42db8fad43cba099488cf030248de4e
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-29 06:06:24 +0100

    feat: add code for the gallery and gallery items

--- src/components/galleryItem/galleryItem.jsx
+++ src/components/galleryItem/galleryItem.jsx
@@ -2,7 +2,10 @@ import './galleryItem.css';
 
 const GalleryItem = ({ item }) => {
   return (
-    <div className='galleryItem'>
+    <div
+      className='galleryItem'
+      style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
+    >
       <img src={item.media} alt={'item' + item.id} />
     </div>
   );
