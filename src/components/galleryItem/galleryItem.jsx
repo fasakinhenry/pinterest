83e6598e9fcc912239bc9427173ee4b99d371950
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-29 08:07:47 +0100

    feat: update and style the gallery item

--- src/components/galleryItem/galleryItem.jsx
+++ src/components/galleryItem/galleryItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import './galleryItem.css';
 
 const GalleryItem = ({ item }) => {
@@ -7,6 +8,16 @@ const GalleryItem = ({ item }) => {
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
       <img src={item.media} alt={'item' + item.id} />
+      <Link to={`/pin/${item.id}`} className='overlay' />
+      <button className='saveButton'>Save</button>
+      <div className='overlayIcons'>
+        <button>
+          <img src='/general/share.svg' alt='share icon' />
+        </button>
+        <button>
+          <img src='/general/more.svg' alt='share icon' />
+        </button>
+      </div>
     </div>
   );
 };
