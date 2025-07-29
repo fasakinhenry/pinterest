56e0b0a0cdfcb1ac996435ea5d91c612a2e198c3
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-29 07:06:42 +0100

    feat: import homepage route in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Homepage from './routes/homepage/homepage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
