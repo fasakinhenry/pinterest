a2333b0e1084729eb32eb0c4c9a8fa21f76f8472
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-30 13:13:02 +0100

    feat: setup imagekit to fetch pins

--- src/components/galleryItem/galleryItem.jsx
+++ src/components/galleryItem/galleryItem.jsx
@@ -8,7 +8,13 @@ const GalleryItem = ({ item }) => {
       className='galleryItem'
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
-      <Image urlEndpoint={urlEndpoint} src={item.media} />
+      {/* <img src={item.media} alt={"item" + item.id} /> */}
+      <Image
+        urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
+        src={item.media}
+        alt={'pin' + item.id}
+        transformation={[{ width: 500, height: 500 }]}
+      />
       <Link to={`/pin/${item.id}`} className='overlay' />
       <button className='saveButton'>Save</button>
       <div className='overlayIcons'>
