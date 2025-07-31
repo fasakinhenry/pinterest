e5c91a3ddf5c851529da19c1be9d81cbc53ad7de
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-31 22:05:29 +0100

    feat: update the postPage with Image

--- src/routes/postPage/postPage.jsx
+++ src/routes/postPage/postPage.jsx
@@ -1,7 +1,17 @@
 import './postPage.css';
+import IKImage from './../../components/image/image';
 
 const PostPage = () => {
-  return <div className='postPage'>PostPage</div>;
+  return (
+    <div className='postPage'>
+      <div className='postContainer'>
+        <div className='postImg'>
+          <IKImage path='/pins/pin1.jpeg' alt='Post Image' w={736} />
+        </div>
+        <div className='postDetails'></div>
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
