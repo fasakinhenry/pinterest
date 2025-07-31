00a02291c85d99573aaafa36e3e6300f702a6c91
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-31 22:13:29 +0100

    feat: create post interaction and user post details

--- src/routes/postPage/postPage.jsx
+++ src/routes/postPage/postPage.jsx
@@ -1,5 +1,7 @@
 import './postPage.css';
 import IKImage from './../../components/image/image';
+import PostInteractions from './../../components/postInteractions/postInteractions';
+import { Link } from 'react-router';
 
 const PostPage = () => {
   return (
@@ -8,7 +10,13 @@ const PostPage = () => {
         <div className='postImg'>
           <IKImage path='/pins/pin1.jpeg' alt='Post Image' w={736} />
         </div>
-        <div className='postDetails'></div>
+        <div className='postDetails'>
+          <PostInteractions />
+          <Link to='/john' className='postUser'>
+            <IKImage path='/general/noAvatar.png' alt='User Avatar' />
+            <span>John Doe</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
