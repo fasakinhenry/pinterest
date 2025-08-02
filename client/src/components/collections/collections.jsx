709b98345c953906ff5c83d594de32a5651da86f
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-02 23:59:36 +0100

    feat: add pseudo content for collections

--- client/src/components/collections/collections.jsx
+++ client/src/components/collections/collections.jsx
@@ -1,7 +1,33 @@
 import './collections.css';
+import IKImage from './../image/image';
 
 const Collections = () => {
-  return <div className='collections'>Collections</div>;
+  return <div className='collections'>
+    {/* Collection */}
+    <div className="collection">
+      <IKImage path="/pins/pin1.jpeg" alt="" />
+      <div className="collectionInfo">
+        <h1>Minimalist Bedroom</h1>
+        <span>12 Pins . 1w</span>
+      </div>
+    </div>
+    {/* Collection */}
+    <div className="collection">
+      <IKImage path="/pins/pin1.jpeg" alt="" />
+      <div className="collectionInfo">
+        <h1>Minimalist Bedroom</h1>
+        <span>12 Pins . 1w</span>
+      </div>
+    </div>
+    {/* Collection */}
+    <div className="collection">
+      <IKImage path="/pins/pin1.jpeg" alt="" />
+      <div className="collectionInfo">
+        <h1>Minimalist Bedroom</h1>
+        <span>12 Pins . 1w</span>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Collections;
