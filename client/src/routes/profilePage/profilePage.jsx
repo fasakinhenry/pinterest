3ec611c9f69badfcea5cbe050690b3ac9d377e9f
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-02 09:17:29 +0100

    Feat: create the profile page component

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,7 +1,27 @@
 import './profilePage.css';
+import IKImage from './../../components/image/image';
 
 const ProfilePage = () => {
-  return <div className='profilePage'>ProfilePage</div>;
+  return (
+    <div className='profilePage'>
+      <IKImage path='general/noAvatar.png' />
+      <h1 className='profileName'>John Doe</h1>
+      <span className='profileUsername'>@johndoe</span>
+      <div className='followCounts'>10 followers . 20 following</div>
+      <div className='profileInteractions'>
+        <IKImage path='general/share.svg' alt='Share Icon' />
+        <div className='profileButtons'>
+          <button>Message</button>
+          <button>Follow</button>
+        </div>
+        <IKImage path='general/more.svg' alt='More Icon' />
+      </div>
+      <div className='profileOptions'>
+        <span>Created</span>
+        <span>Saved</span>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
