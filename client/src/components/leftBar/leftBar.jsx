adbe4ec9433a2b8bc358eb7483dde01110597f66
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-03 00:09:01 +0100

    feat: change a to link in LeftBar

--- client/src/components/leftBar/leftBar.jsx
+++ client/src/components/leftBar/leftBar.jsx
@@ -1,29 +1,30 @@
 import IKImage from '../image/image';
+import { Link } from 'react-router-dom';
 import './leftBar.css';
 
 const LeftBar = () => {
   return (
     <div className='leftBar'>
       <div className='menuIcons'>
-        <a href='/' className='menuIcon'>
+        <Link to='/' className='menuIcon'>
           <IKImage path='/general/logo.png' alt='logo' className='logo' />
-        </a>
-        <a href='/' className='menuIcon'>
+        </Link>
+        <Link to='/' className='menuIcon'>
           <IKImage path='/general/home.svg' alt='home' />
-        </a>
-        <a href='/' className='menuIcon'>
+        </Link>
+        <Link to='/' className='menuIcon'>
           <IKImage path='/general/create.svg' alt='create' />
-        </a>
-        <a href='/' className='menuIcon'>
+        </Link>
+        <Link to='/' className='menuIcon'>
           <IKImage path='/general/updates.svg' alt='updates' />
-        </a>
-        <a href='/' className='menuIcon'>
+        </Link>
+        <Link to='/' className='menuIcon'>
           <IKImage path='/general/messages.svg' alt='messages' />
-        </a>
+        </Link>
       </div>
-      <a href='/' className='menuIcon'>
+      <Link to='/' className='menuIcon'>
         <IKImage path='/general/settings.svg' alt='messages' />
-      </a>
+      </Link>
     </div>
   );
 };
