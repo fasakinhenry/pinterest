081d9e3ef86455f74ae339ad28349ad412ba17bf
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-04 10:22:29 +0100

    test: create test API route handler

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.use('/test', (req, res) => {
+  return res.json('Hello from backend API');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
