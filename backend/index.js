079f2984c84c12fe74610724b77fa2281b04ae4b
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-04 11:02:34 +0100

    feat: update all controllers with pseudo element

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import userRouter from "./routes/user.route.js"
+import pinRouter from "./routes/pin.route.js"
+import boardRouter from "./routes/board.route.js"
+import commentRouter from "./routes/comment.route.js"
 
 const app = express();
 
 app.use('/users', userRouter);
+app.use('/pins', pinRouter);
+app.use('/boards', boardRouter);
+app.use('/comments', commentRouter);
 
 const PORT = process.env.PORT || 3000;
 
