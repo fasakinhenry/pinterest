c75c95817778fbaba0fab935ae6263e96bed745a
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-04 10:57:46 +0100

    feat: add all needed routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { test } from '../controllers/user.controller';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get("/users", test);
+router.get('/test', test);
 
 export default router;
