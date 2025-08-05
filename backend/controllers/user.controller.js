16409ed69625e7d76eabe1e2af6d401bd2a440f7
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 21:30:33 +0100

    feat: create a user controller to get a user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,16 @@
-export const test = (req, res) => {
-  return res.json('Hello from controller');
+import User from '../models/user.model.js';
+
+export const getUser = async (req, res) => {
+  const {username} = req.params;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const {hashedPassword, ...detailsWithoutPassword} = user.toObject();
+    res.status(200).json(detailsWithoutPassword);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
