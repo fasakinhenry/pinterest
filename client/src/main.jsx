648d81339a060275817ebb8c7d3edd790891d7e5
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 10:32:09 +0100

    feat(fe): add the tanstack react query to the main jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,20 +9,25 @@ import AuthPage from './routes/authPage/authPage';
 import ProfilePage from './routes/profilePage/profilePage';
 import SearchPage from './routes/searchPage/searchPage';
 import MainLayout from './layouts/mainLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/create' element={<CreatePage />} />
-          <Route path='/pin/:id' element={<PostPage />} />
-          <Route path='/:username' element={<ProfilePage />} />
-          <Route path='/search' element={<SearchPage />} />
-        </Route>
-        <Route path='/auth' element={<AuthPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/create' element={<CreatePage />} />
+            <Route path='/pin/:id' element={<PostPage />} />
+            <Route path='/:username' element={<ProfilePage />} />
+            <Route path='/search' element={<SearchPage />} />
+          </Route>
+          <Route path='/auth' element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
