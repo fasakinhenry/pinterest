6498af577043ee4451579be0ef5c1cc843c80bd5
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 19:33:35 +0100

    feat: add limit to pins fetched

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -1,7 +1,7 @@
 import Pin from '../models/pin.model.js';
 
 export const getPins = async (req, res) => {
-  const pins = await Pin.find();
+  const pins = await Pin.find().limit(21).skip(21);
 
   res.status(200).json(pins);
 };
