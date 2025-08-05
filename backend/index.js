3bcaa224d05dffcee63a3f84f8eed935932d908c
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 08:28:27 +0100

    feat: use utility in index file

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import userRouter from "./routes/user.route.js"
 import pinRouter from "./routes/pin.route.js"
 import boardRouter from "./routes/board.route.js"
 import commentRouter from "./routes/comment.route.js"
+import connectDB from './utils/connectDB.js';
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use('/comments', commentRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
