4ddd14b42d7eede9ff5da4af9a8f3c7619892b18
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 09:31:28 +0100

    feat: create a route to handle user info

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import userRouter from "./routes/user.route.js"
-import pinRouter from "./routes/pin.route.js"
-import boardRouter from "./routes/board.route.js"
-import commentRouter from "./routes/comment.route.js"
+import userRouter from './routes/user.route.js';
+import pinRouter from './routes/pin.route.js';
+import boardRouter from './routes/board.route.js';
+import commentRouter from './routes/comment.route.js';
 import connectDB from './utils/connectDB.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/users', userRouter);
 app.use('/pins', pinRouter);
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,6 +3,11 @@ import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
+router.post('/create', (req, res) => {
+  const userInformation = req.body;
+  return res.json(userInformation);
+});
+
 router.get('/test', test);
 
 export default router;
