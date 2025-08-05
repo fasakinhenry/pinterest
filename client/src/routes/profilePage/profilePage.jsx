58f3894ae262b8d5765cefee1d0212f65d22b18a
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 21:22:17 +0100

    chore: setup fetching from backend in profile page

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -3,9 +3,24 @@ import IKImage from './../../components/image/image';
 import { useState } from 'react';
 import Gallery from './../../components/gallery/gallery';
 import Collections from '../../components/collections/collections';
+import { useQuery } from '@tanstack/react-query';
+import apiRequest from '../../utils/apiRequest';
 
 const ProfilePage = () => {
   const [type, setType] = useState('saved');
+
+  const { username } = useParams();
+  
+  const { isPending, error, data } = useQuery({
+    queryKey: ['pin', username],
+    queryFn: () => apiRequest.get(`/users/${username}`).then((res) => res.data),
+  });
+
+  if (isPending) return 'Loading...';
+  if (error) return 'An error has occured: ' + error.message;
+
+  if (!data) return 'User not found!';
+
   return (
     <div className='profilePage'>
       <IKImage
