b4f46053f8d2b6ee2c08d6960201a6e514f145d4
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 20:39:01 +0100

    feat: create endpoint to get details of single pin

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -4,17 +4,33 @@ export const getPins = async (req, res) => {
   const pageNumber = Number(req.query.cursor) || 0;
   const search = req.query.search;
   const LIMIT = 21;
-  const pins = await Pin.find({
-    $or: [
-      { title: { $regex: search, $options: 'i' } },
-      {tags: {$in: [search]}},
-      { description: { $regex: search, $options: 'i' }},
-    ],
-  })
+  const pins = await Pin.find(
+    search
+      ? {
+          $or: [
+            { title: { $regex: search, $options: 'i' } },
+            { tags: { $in: [search] } },
+            { description: { $regex: search, $options: 'i' } },
+          ],
+        }
+      : {}
+  )
     .limit(LIMIT)
     .skip(pageNumber * LIMIT);
 
   const hasNextPage = pins.length === LIMIT;
 
-  res.status(200).json({pins, nextCursor: hasNextPage ? pageNumber + 1 : null});
+  res
+    .status(200)
+    .json({ pins, nextCursor: hasNextPage ? pageNumber + 1 : null });
+};
+
+export const getPin = async (req, res) => {
+  const { id } = req.params;
+
+  const pin = await Pin.findById(id).populate('user', 'username img displayName');
+  if (!pin) {
+    return res.status(404).json({ message: 'Pin not found' });
+  }
+  res.status(200).json(pin);
 };
