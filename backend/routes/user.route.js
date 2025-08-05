05856ff0084bc367c6f5dd3add4a2a798b673c95
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 09:41:15 +0100

    feat: update the user route to create user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
+import User from '../models/user.model.js';
 
 const router = express.Router();
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const userInformation = req.body;
+  await User.create({
+    displayName: userInformation.displayName,
+    userName: userInformation.userName,
+    email: userInformation.email,
+    hashedPassword: userInformation.hashedPassword,
+    img: userInformation.img,
+  });
   return res.json(userInformation);
 });
 
