e3ac2a7a522e20cd7387693d5f1157535b31a57f
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 08:26:53 +0100

    chore: add logging to mongogb connection

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
     process.exit(1);
