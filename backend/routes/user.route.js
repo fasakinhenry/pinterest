d5818377020c9dc0ddc757de7b49e94984990300
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 09:53:39 +0100

    Feat: create endpoint for getting all users

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
   const userInformation = req.body;
+
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
   await User.create({
     displayName: userInformation.displayName,
-    userName: userInformation.userName,
+    username: userInformation.username,
     email: userInformation.email,
-    hashedPassword: userInformation.hashedPassword,
+    hashedPassword: hashedPassword,
   });
   return res.json(userInformation);
+});
+
+router.get('/fetch', async (req, res) => {
+  const users = await User.find();
+  return res.json(users);
 });
 
 router.get('/test', test);
