f996a6254022787072273c56af148ff9f7afeb16
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-05 20:18:31 +0100

    feat: update search to populate query params

--- client/src/components/topBar/topBar.jsx
+++ client/src/components/topBar/topBar.jsx
@@ -1,15 +1,21 @@
 import './topBar.css';
 import UserButton from '../userButton/userButton';
 import IKImage from './../image/image';
+import { useNavigate } from 'react-router';
 
 const TopBar = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?search=${e.target[0].value}`);
+  };
   return (
     <div className='topBar'>
       {/* Search */}
-      <div className='search'>
+      <form onSubmit={handleSubmit} className='search'>
         <IKImage path='/general/search.svg' alt='search' />
         <input type='text' placeholder='search' />
-      </div>
+      </form>
       {/* User */}
       <UserButton />
     </div>
