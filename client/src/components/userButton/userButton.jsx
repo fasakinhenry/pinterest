7e4816358ed765e8a863b9f2dac2ca96e327d089
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-08 23:05:49 +0100

    fix: fix the user button dropdown

--- client/src/components/userButton/userButton.jsx
+++ client/src/components/userButton/userButton.jsx
@@ -11,17 +11,16 @@ const UserButton = () => {
   return currentUser ? (
     <div className='userButton'>
       <IKImage path='/general/noAvatar.png' alt='avatar' />
-      <IKImage
-        onClick={() => setOpen((prev) => !prev)}
-        path='/general/arrow.svg'
-        alt='arrow'
-        className='arrow'
-      />
-      {open && <div className='userOptions'>
-        <div className='userOption'>Profile</div>
-        <div className='userOption'>Settings</div>
-        <div className='userOption'>Logout</div>
-      </div>}
+      <div onClick={() => setOpen((prev) => !prev)}>
+        <IKImage path='/general/arrow.svg' alt='arrow' className='arrow' />
+      </div>
+      {open && (
+        <div className='userOptions'>
+          <div className='userOption'>Profile</div>
+          <div className='userOption'>Settings</div>
+          <div className='userOption'>Logout</div>
+        </div>
+      )}
     </div>
   ) : (
     <a href='/' className='loginLink'>
