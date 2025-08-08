c435190ec77e6b003f5c58d30db49aeca49260fc
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-08 22:46:27 +0100

    feat: create token-based login controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,9 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req, res) => {
-  const {username, displayName, email, password} = req.body;
+  const { username, displayName, email, password } = req.body;
 
   if (!username || !displayName || !email || !password) {
     return res.status(400).json({ message: 'All fields are required' });
@@ -17,26 +18,50 @@ export const registerUser = async (req, res) => {
     hashedPassword: newHashedPassword,
   });
 
-  const {hashedPassword, ...detailsWithoutPassword} = user.toObject();
+  const { hashedPassword, ...detailsWithoutPassword } = user.toObject();
   res.status(201).json(detailsWithoutPassword);
 };
 
 export const loginUser = async (req, res) => {
-  
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.hashedPassword);
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+
+  const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET);
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  const { hashedPassword, ...detailsWithoutPassword } = user.toObject();
+  res.status(200).json(detailsWithoutPassword);
 };
-export const logoutUser = async (req, res) => {
-  
-};
+export const logoutUser = async (req, res) => {};
 
 export const getUser = async (req, res) => {
-  const {username} = req.params;
+  const { username } = req.params;
 
   try {
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-    const {hashedPassword, ...detailsWithoutPassword} = user.toObject();
+    const { hashedPassword, ...detailsWithoutPassword } = user.toObject();
     res.status(200).json(detailsWithoutPassword);
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
