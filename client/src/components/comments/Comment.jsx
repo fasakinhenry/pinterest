38644e2629bcfba74f637b2d3fea46edfd1712ea
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-08 07:00:07 +0100

    feat: create the comment component

--- client/src/components/comments/Comment.jsx
+++ client/src/components/comments/Comment.jsx
@@ -1,15 +1,16 @@
-import IKImage from "../image/image";
-import { format } from "timeago.js";
+import IKImage from '../image/image';
+import { format } from 'timeago.js';
 
 const Comment = ({ comment }) => {
   return (
     <div className='comment'>
-      <IKImage path={comment.user.img || '/general/noAvatar.png'} alt='avatar' />
+      <IKImage
+        path={comment.user.img || '/general/noAvatar.png'}
+        alt='avatar'
+      />
       <div className='commentContent'>
-        <span className='commentUsername'>{comment.user.username}</span>
-        <p className='commentText'>
-          {comment.description}
-        </p>
+        <span className='commentUsername'>{comment.user.displayName}</span>
+        <p className='commentText'>{comment.description}</p>
         <span className='commentTime'>{format(comment.createdAt)}</span>
       </div>
     </div>
