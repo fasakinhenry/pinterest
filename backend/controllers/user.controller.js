09d777ca51f2125749e5a3d6ed8bfb0c25c16af6
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-08 08:28:16 +0100

    feat: create the register user endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,32 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export const registerUser = async (req, res) => {
+  const {username, displayName, email, password} = req.body;
+
+  if (!username || !displayName || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  const newHashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    displayName,
+    email,
+    hashedPassword: newHashedPassword,
+  });
+
+  const {hashedPassword, ...detailsWithoutPassword} = user.toObject();
+  res.status(201).json(detailsWithoutPassword);
+};
+
+export const loginUser = async (req, res) => {
+  
+};
+export const logoutUser = async (req, res) => {
+  
+};
 
 export const getUser = async (req, res) => {
   const {username} = req.params;
