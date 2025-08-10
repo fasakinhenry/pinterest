5c4b1525e39cf7c7d941783671d7fb44f4e79140
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-10 23:27:09 +0100

    feat: add state for emoji picker in commentform

--- client/src/components/comments/commentForm.jsx
+++ client/src/components/comments/commentForm.jsx
@@ -4,14 +4,20 @@ import { useState } from 'react';
 
 const CommentForm = () => {
   const [open, setOpen] = useState(false);
+  const [desc, setDesc] = useState('');
+
+  const handleEmojiClick = (emoji) => {
+    setDesc((prev) => prev + emoji.emoji);
+    setOpen(false);
+  };
   return (
     <form className='commentForm'>
-      <input type='text' placeholder='Add a comment' />
+      <input type='text' placeholder='Add a comment' onChange={(e) => setDesc(e.target.value)} value={desc} />
       <div className='emoji'>
         <div onClick={() => setOpen((prev) => !prev)}>😅</div>
         {open && (
           <div className='emojiPicker'>
-            <EmojiPicker />
+            <EmojiPicker onEmojiClick={handleEmojiClick} />
           </div>
         )}
       </div>
