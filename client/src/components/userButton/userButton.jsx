7b3a1aa5600a245e8a79a638df9963f4cfaf8102
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-10 10:41:15 +0100

    chore: install zustand
    finish logout feature

--- client/src/components/userButton/userButton.jsx
+++ client/src/components/userButton/userButton.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import './userButton.css';
 import IKImage from '../image/image';
+import apiRequest from './../../utils/apiRequest';
+import { useNavigate } from 'react-router';
 
 const UserButton = () => {
   const [open, setOpen] = useState(false);
 
+  const navigate = useNavigate();
+
   // TEMP
   const currentUser = true;
 
+  const handleLogout = async () => {
+    try {
+      await apiRequest.post('/users/auth/logout', {});
+      navigate('/auth');
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return currentUser ? (
     <div className='userButton'>
       <IKImage path='/general/noAvatar.png' alt='avatar' />
@@ -18,7 +30,9 @@ const UserButton = () => {
         <div className='userOptions'>
           <div className='userOption'>Profile</div>
           <div className='userOption'>Settings</div>
-          <div className='userOption'>Logout</div>
+          <div className='userOption' onClick={handleLogout}>
+            Logout
+          </div>
         </div>
       )}
     </div>
