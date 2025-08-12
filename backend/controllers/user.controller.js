f2dde089d62d11691e62f336ccfc90cfa1788422
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-12 11:10:29 +0100

    Feat: remove the unwanted cookie same strict setting

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -53,7 +53,6 @@ export const loginUser = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
