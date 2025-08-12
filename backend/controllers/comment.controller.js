020fbc7ce4bb4556a44ab15b2d99d7762463ff91
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-12 01:53:12 +0100

    fix: fix jwt import in verifyToken

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,6 +1,5 @@
 import Comment from '../models/comment.model.js';
 import User from '../models/user.model.js';
-import jwt from 'jsonwebtoken';
 
 export const getPostComments = async (req, res) => {
   const { postId } = req.params;
