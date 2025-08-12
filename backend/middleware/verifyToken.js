785d66360958c7afbf7954801358049d3c74b4e1
Fasakin Henry <fasakinhenry@gmail.com>
2025-08-12 10:15:15 +0100

    fix: troubleshoot middleware error

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  
+
   if (!token) return res.status(401).json({ message: 'Not authenticated' });
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getPostComments, addComment } from '../controllers/comment.controller.js';
+import {
+  getPostComments,
+  addComment,
+} from '../controllers/comment.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/:postId', getPostComments);
-router.post("/", verifyToken, addComment);
+router.post('/', addComment);
 
 export default router;
